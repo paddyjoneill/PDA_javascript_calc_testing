daab8965de872f26e99336b2508e6a7bfd40ba00
Patrick O'Neill <paddyjoneill@hotmail.com>
2020-03-03 19:20:14 +0000

    done

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -12,27 +12,36 @@ describe('calculator', function () {
   })
 
   it('it can add', function() {
-    calculator.previousTotal = 1
-    calculator.add(4)
-    assert.equal(calculator.runningTotal, 5)
+    calculator.numberClick(1)
+    calculator.operatorClick('+')
+    calculator.numberClick(4)
+    calculator.operatorClick('=')
+    assert.equal(calculator.previousTotal, 5)
   })
 
   it('it can subtract', function() {
-    calculator.previousTotal = 7
-    calculator.subtract(4)
-    assert.equal(calculator.runningTotal, 3)
+    calculator.numberClick(7)
+    calculator.operatorClick('-')
+    calculator.numberClick(4)
+    calculator.operatorClick('=')
+    assert.equal(calculator.previousTotal, 3)
   })
 
   it('it can multiply', function() {
-    calculator.previousTotal = 3
-    calculator.multiply(5)
-    assert.equal(calculator.runningTotal, 15)
+    calculator.numberClick(3)
+    calculator.operatorClick('*')
+    calculator.numberClick(5)
+    calculator.operatorClick('=')
+    assert.equal(calculator.previousTotal, 15)
   })
 
   it('it can divide', function() {
-    calculator.previousTotal = 21
-    calculator.divide(7)
-    assert.equal(calculator.runningTotal, 3)
+    calculator.numberClick(2)
+    calculator.numberClick(1)
+    calculator.operatorClick('/')
+    calculator.numberClick(7)
+    calculator.operatorClick('=')
+    assert.equal(calculator.previousTotal, 3)
   })
 
   it('it can concatenate multiple number clicks', function() {
@@ -42,10 +51,27 @@ describe('calculator', function () {
   })
 
   it('it can chain multiple operations', function() {
-    calculator.previousTotal = 3
-    calculator.multiply(8)
-    calculator.divide(6)
-    assert.equal( calculator.runningTotal  , 4 )
+    calculator.numberClick(3)
+    calculator.operatorClick('*')
+    calculator.numberClick(8)
+    calculator.operatorClick('/')
+    calculator.numberClick(6)
+    calculator.operatorClick('=')
+    assert.equal( calculator.previousTotal  , 4 )
   })
+
+
+  it('clear the running total without affecting the calculation', function() {
+    calculator.numberClick(1)
+    calculator.numberClick(2)
+    calculator.clearClick()
+    calculator.numberClick(3)
+    calculator.operatorClick('*')
+    calculator.numberClick(8)
+    calculator.operatorClick('=')
+    assert.equal( calculator.previousTotal  , 24 )
+  })
+
+
 
 });
